62107d052a1b5ad40f895d3e1fac1b03ce605a52
chacha <sarochachangdii@gmail.com>
2020-10-07 17:47:12 +0700

    commit

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Chacha World" />
+    <HelloWorld msg="Welcome to Artisan Cinema" />
   </div>
 </template>
 
@@ -40,5 +40,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: darkblue;
 }
 </style>
