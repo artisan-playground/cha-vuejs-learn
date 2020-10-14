e9cb40d0a4a1373db176c1c51c4cf3b574674909
chacha <sarochachangdii@gmail.com>
2020-10-14 17:32:26 +0700

    Plll

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Artisan Cinema" />
+    <HelloWorld msg="Welcome to Portfolio Of Chacha" />
   </div>
 </template>
 
